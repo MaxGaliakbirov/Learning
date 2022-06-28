1a32f7b29b8dbf280aeede8674cb9b64f6507fbd
mf.galiakbirov@gmail.com <n19869400>
2022-06-29 00:41:44 +0300

    solution task to read and write to file

--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,20 @@
 import java.io.File
+import java.lang.System.out
 
 
 fun main () {
     val file = File("./src/input.txt")
-    val line = file.bufferedReader().readLine()
-    println(line)
+    val inLine = file.bufferedReader().readLine() //прочитали файл
+
+    val a = inLine.substringBefore(" ").toInt() //взяли первое значение по индксу
+    val b = inLine.substringAfter(" ").toInt()
+    val result = a + b
+    val outLine = File("./src/output.txt")
+    outLine.writeText(result.toString()) //записали в файл
+
+}
+
+
 //   var line = scan.nextLine()
 //    val scan = java.util.Scanner(System.`in`)
 //    var line = scan.nextLine()
@@ -14,4 +24,4 @@ fun main () {
 //    val a = scan.nextInt()
 //    val b = scan.nextInt()
 //    println(a + b)
-}
+
