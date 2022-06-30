2fcb2506a31b7591f38eb8c429462644e5849ab2
mf.galiakbirov@gmail.com <n19869400>
2022-06-30 22:21:08 +0300

    solution task to Stepik2

--- src/toUpper.kt
+++ src/toUpper.kt
@@ -28,4 +28,22 @@ fun main () {
 //*****************************
 //    val str = scan.nextLine()
 //    println("${str[2]}${str[1]}${str[0]}")
+//Напишите программу, которая считывает два трёхзначных числа и складывает первый и третий разряд
+//каждого из них. Например, если введены числа 191191 и 292292, ответом будет число 6 (1 + 1 + 2 + 2 = 61+1+2+2=6).
+//****************************
+// val str = scan.nextLine().toString()
+// val str1 = str.substringBefore(" ")
+// val str2 = str.substringAfter(" ")
+// val a = str1[0].toString().toInt()
+// val b = str1[2].toString().toInt()
+// val c = str2[0].toString().toInt()
+// val d = str2[2].toString().toInt()
+// println(a+b+c+d)
+//Напишите программу, которая считывает число от 1000 до 9999, разворачивает его и выводит без ведущих нулей:
+//1300 -> 0031 -> 31 // т.е. нули спереди числа - ведущие нули
+//****************************
+
+  val numberRead = scan.nextLine().reversed().substringAfter("0").toInt()
+ println(numberRead)
+
  }
